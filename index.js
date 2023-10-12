2a2bcc366f5e5af2bf241275c410008aab867cc3
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-10-11 20:35:01 -0400

    create myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+
+  const messages = writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+  console.log(messages);
+  
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  countDown(10);
+  
